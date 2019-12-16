bc7ef9849e2c5ac84873255252efd882adf3cc07
Ronak Raithatha <ronaksraithatha@gmail.com>
2019-12-15 22:55:28 -0500

    added about page and events / slug pages

--- components/nav.js
+++ components/nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
@@ -15,6 +15,9 @@ const Nav = () => (
       <li>
         <Link href="/">
           <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
         </Link>
       </li>
       {links.map(({ key, href, label }) => (
